18525c19ad0aba6b880337c266eae772fed6aabe
aslanyi <mustafatahatepedelen@gmail.com>
2019-09-27 18:45:17 +0300

    added page config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   // some configuration
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    }
+  },
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://aslanyi.github.io/nextjs-styled-component-example/' : '',
   // another configuration
 };
