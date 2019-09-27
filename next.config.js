65f438e5901fdb24f2c2cdd032b37f978e1fd69f
aslanyi <mustafatahatepedelen@gmail.com>
2019-09-27 17:45:25 +0300

    added next config file

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // some configuration
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/https://aslanyi.github.io/nextjs-styled-component-example/' : '',
+  // another configuration
+};
